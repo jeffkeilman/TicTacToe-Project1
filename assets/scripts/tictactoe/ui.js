4e916f9fd3dc62d405b66a623cfcb406cc370ee3
jeffkeilman <jkeilman93@gmail.com>
2017-09-27 13:18:10 -0400

    Add success and failure
    Add success and failure handling for login

--- assets/scripts/tictactoe/ui.js
+++ assets/scripts/tictactoe/ui.js
@@ -1,8 +1,21 @@
 'use strict'
 const store = require('../store.js')
 
-const signOnSucces = function (data) {
-  //display game
+const signOnSuccess = function (data) {
+  // TODO: Display game
 
   store.user = data.user
+}
+
+const signOnFailure = function () {
+  $('#displayFeedback').html(
+    '<div class="alert alert-danger" role="alert">Hmm... looks like we don\'t have an account with that email and password.<div>'
+  )
+  $('#myModal').modal('hide')
+  $('#passwordLabel').val('')
+}
+
+module.exports = {
+  signOnSuccess,
+  signOnFailure
 }
